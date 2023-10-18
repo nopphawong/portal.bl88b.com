dec4e318c22b71407acceea1814624753d0bfce5
AlexKhonshu <nopphawong.n@gmail.com>
2023-10-18 14:10:39 +0700

    first commit

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use CodeIgniter\HTTP\CLIRequest;
+use CodeIgniter\HTTP\IncomingRequest;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use App\Libraries\CustomView;
+
+/**
+ * Class BaseController
+ *
+ * BaseController provides a convenient place for loading components
+ * and performing functions that are needed by all your controllers.
+ * Extend this class in any new controllers:
+ *     class Home extends BaseController
+ *
+ * For security be sure to declare any new methods as protected or private.
+ */
+abstract class BaseController extends Controller
+{
+    /**
+     * Instance of the main Request object.
+     *
+     * @var CLIRequest|IncomingRequest
+     */
+    protected $request;
+
+    /**
+     * An array of helpers to be loaded automatically upon
+     * class instantiation. These helpers will be available
+     * to all other controllers that extend BaseController.
+     *
+     * @var array
+     */
+    protected $helpers = [];
+    protected $headerInfo = [];
+    protected $viewData = [];
+
+    /**
+     * Custom the view.
+     * @var CustomView
+     */
+    protected $cv;
+
+    /**
+     * Be sure to declare properties for any property fetch you initialized.
+     * The creation of dynamic property is deprecated in PHP 8.2.
+     */
+    // protected $session;
+
+    /**
+     * @return void
+     */
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+
+        helper('html');
+
+        // Custom View.
+        $this->cv = new CustomView();
+
+        // Set headers.
+        $this->headerInfo['title'] = 'UFA PORTAL';
+    }
+}
---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,32 @@
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <?= script_tag("assets/plugins/jquery/jquery.min.js") ?>
+  <!-- Bootstrap 4 -->
+  <?= script_tag("assets/plugins/bootstrap/js/bootstrap.bundle.min.js") ?>
+  <!-- AdminLTE App -->
+  <?= script_tag("assets/dist/js/adminlte.js") ?>
+
+  <!-- OPTIONAL SCRIPTS -->
+  <!-- ChartJS -->
+  <?= script_tag("assets/plugins/chart.js/Chart.min.js") ?>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <?= script_tag("assets/dist/js/pages/dashboard3.js") ?>
+  </body>
+
+  </html>
