63617938c204623bfb8a64d1399bb067767adc3a
ittibl <ittipat.bl@gmail.com>
2023-10-22 19:47:50 +0700

    fix logo

--- app/Views/adminlte/layouts/_sidebar.php
+++ app/Views/adminlte/layouts/_sidebar.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
-          <img src="<?= base_url() ?>assets/dist/images/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
           <span class="brand-text font-weight-light">AdminLTE 3</span>
       </a>
 
