64b2b00753b49d9b0dd542ce41dba2362391b12c
AlexKhonshu <nopphawong.n@gmail.com>
2023-10-23 17:04:19 +0700

    login title

--- app/Views/adminlte/pages/login.php
+++ app/Views/adminlte/pages/login.php
@@ -7,7 +7,7 @@
         <div class="card">
             <div class="card-body login-card-body">
                 <div class="login-logo">
-                    <a><b>BL88B</b></a>
+                    <a>Admin <b>Protal</b></a>
                 </div>
                 <form @submit="submit">
                     <div class="input-group mb-3">
