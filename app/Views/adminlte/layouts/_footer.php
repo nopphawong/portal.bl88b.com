84499cdb912bce7670496694493ae830a6544cc3
AlexKhonshu <nopphawong.n@gmail.com>
2023-10-31 22:07:38 +0700

    improve ui

--- app/Views/adminlte/layouts/_footer.php
+++ app/Views/adminlte/layouts/_footer.php
@@ -2,7 +2,7 @@
     <strong>Copyright &copy; 2023-2024 <a>bl88b.com</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
+        <b>Version</b> 1.2.0
     </div>
 </footer>
 
