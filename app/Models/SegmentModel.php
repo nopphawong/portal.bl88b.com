ed237601f4667b065bcbcbb355ca446c1820676d
mbaitti <ittipat.scit@gmail.com>
2024-01-02 11:21:55 +0700

    reformat

--- app/Models/SegmentModel.php
+++ app/Models/SegmentModel.php
@@ -7,6 +7,6 @@ use CodeIgniter\Model;
 class SegmentModel extends Model
 {
     protected $table = 'tb_segment';
-    protected $allowedFields = ['agent', 'wheel', 'index','title','type','value','rate','hex','image', 'status', 'add_date', 'add_by', 'edit_date', 'edit_by'];
+    protected $allowedFields = ['agent', 'wheel', 'index', 'title', 'type', 'value', 'rate', 'hex', 'image', 'status', 'add_date', 'add_by', 'edit_date', 'edit_by'];
     protected $returnType = 'object';
 }
